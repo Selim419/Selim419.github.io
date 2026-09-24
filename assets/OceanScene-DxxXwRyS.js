31293ad8a178c40aa8c6f3855af684b0526bbf23
Selim419 <selimisik3190@gmail.com>
2026-09-24 21:41:32 +0300

    Remove plain HTML version

--- assets/OceanScene-DxxXwRyS.js
+++ assets/OceanScene-DxxXwRyS.js
@@ -1,4 +1,4 @@
-import{r as da,j as il}from"./index-Bp16FATz.js";/**
+import{r as da,j as il}from"./index-CUpgv3Mo.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
